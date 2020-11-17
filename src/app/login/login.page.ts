f9e06eb4788062a7d58842974997bd586fa7221a
Thiago Tassinari <thiagotassinari12@gmail.com>
2020-11-17 15:19:22 -0300

    Correção da instância do método group

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public formLogin: FormGroup;
+
+  public mensagens_validacao = {
+    email: [
+      { tipo: 'required', mensagem: 'O campo E-mail é obrigatório!' },
+      { tipo: 'email', mensagem: 'E-mail inválido!' }
+    ],
+    senha: [
+      { tipo: 'required', mensagem: 'O campo senha é obrigatório!' },
+      { tipo: 'minLength', mensagem: 'A senha deve ter pelo menos 6 caracteres!' }
+    ]
+  };
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.formLogin = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+    })
+  }
 
   ngOnInit() {
   }
 
+  public login() {
+    if(this.formLogin.valid) {
+      console.log('Formulário válido!');
+    } else {
+      console.log('Formulário inválido!');
+      
+    }
+  }
 }
